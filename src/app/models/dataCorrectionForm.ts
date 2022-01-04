9a32f46e1150076f4c96a80510cbf61cbae35947
Frederick Katsura <katsurinstudios@protonmail.ch>
2022-01-04 02:11:07 -0400

    Removed One Release Date Field in Correction Form
    An ISBN entry can only have one release date and doesn't track both physical and digital releases.

--- src/app/models/dataCorrectionForm.ts
+++ src/app/models/dataCorrectionForm.ts
@@ -1,8 +1,7 @@
 export class dataCorrectionForm {
 	isbn: string | undefined;
 	title: string | undefined;
-	digitalRelease: string | undefined;
-	physicalRelease: string | undefined;
+	release: string | undefined;
 	coverURL: string | undefined;
 	storeURLs: string[] | undefined;
 	bookType: string | undefined;
