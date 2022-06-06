f0b26163420ae0aeb43e67b54f80928d7a1e19f1
Frederick Katsura <katsurinstudios@protonmail.ch>
2022-06-06 18:52:42 -0300

    Created Series Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomePageComponent } from "./home-page/home-page.component";
 import { ImprintPageComponent } from "./imprint-page/imprint-page.component";
 import { ImprintIndexComponent } from "./imprint-index/imprint-index.component";
 import { MissingPageComponent } from "./missing-page/missing-page.component";
+import { SeriesPageComponent } from "./series-page/series-page.component";
 
 const routes: Routes = [
 	{ path: "", component: HomePageComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
 	{ path: "publisher", component: ImprintIndexComponent },
 	{ path: "publisher/:id", component: ImprintPageComponent },
 	{ path: "support", component: SupportPageComponent },
+	{ path: "series/:id", component: SeriesPageComponent },
 	{ path: "**", pathMatch: "full", component: MissingPageComponent }
 ];
 
---
+++ src/app/series-page/series-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-series-page",
+	templateUrl: "./series-page.component.html",
+	styleUrls: ["./series-page.component.css"]
+})
+export class SeriesPageComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
