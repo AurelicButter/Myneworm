9f90948440669e5b09e42bb7aad3cff3443ff4e7
Frederick Katsura <katsurinstudios@protonmail.ch>
2022-07-08 19:33:26 -0300

    Updated FAQ to Contain Text and Minimally Styled Page

--- src/app/faq-page/faq-page.component.ts
+++ src/app/faq-page/faq-page.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FAQData } from "./faqData";
 
 @Component({
 	selector: "app-faq-page",
 	templateUrl: "./faq-page.component.html",
 	styleUrls: ["./faq-page.component.css"]
 })
-export class FaqPageComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
+export class FaqPageComponent {
+	faqData = FAQData;
 }
