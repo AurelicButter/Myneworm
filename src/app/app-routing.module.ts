bd134b9645f1d36ed98a6a17c4c892c98c25ce54
Frederick Katsura <katsurinstudios@protonmail.ch>
2022-08-17 03:23:30 -0300

    Added About Page and Static Page Title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ImprintIndexComponent } from "./imprint-index/imprint-index.component";
 import { MissingPageComponent } from "./missing-page/missing-page.component";
 import { SeriesPageComponent } from "./series-page/series-page.component";
 import { FaqPageComponent } from "./faq-page/faq-page.component";
+import { AboutPageComponent } from "./about-page/about-page.component";
 
 const routes: Routes = [
 	{ path: "", component: HomePageComponent },
@@ -17,6 +18,7 @@ const routes: Routes = [
 	{ path: "publisher/:id", component: ImprintPageComponent },
 	{ path: "support", component: SupportPageComponent },
 	{ path: "series/:id", component: SeriesPageComponent },
+	{ path: "about", component: AboutPageComponent },
 	{ path: "faq", component: FaqPageComponent },
 	{ path: "**", pathMatch: "full", component: MissingPageComponent }
 ];
