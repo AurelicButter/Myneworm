36557d7b4a1000de86c9226b93b5edcaaa3eec24
Frederick Katsura <fkatsura@katsurin.com>
2023-02-03 04:16:31 -0500

    Started Work on the Search Page

---
+++ src/app/search-page/search-page.component.ts
@@ -0,0 +1,44 @@
+import { Component } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { ActivatedRoute } from "@angular/router";
+import { BookData } from "../models/bookData";
+import { MetadataService } from "../services/metadata.service";
+import { MynewormAPIService } from "../services/myneworm-api.service";
+import { UtilitiesService } from "../services/utilities.service";
+
+@Component({
+	selector: "search-page",
+	templateUrl: "./search-page.component.html",
+	styleUrls: ["./search-page.component.css"]
+})
+export class SearchPageComponent {
+	displayedColumns = ["cover", "title", "format", "type"];
+	searchTerm = "";
+	public dataSource: MatTableDataSource<BookData> = new MatTableDataSource<BookData>();
+
+	constructor(
+		private route: ActivatedRoute,
+		public service: MynewormAPIService,
+		public utilities: UtilitiesService,
+		private metaService: MetadataService
+	) {
+		this.metaService.updateMetaTags("Search", "/search");
+	}
+
+	private searchBooks() {
+		if (this.searchTerm === "") {
+			return;
+		}
+		this.service.searchBooksWithLimit(this.searchTerm, 25, 1).subscribe((data: BookData[]) => {
+			this.dataSource = new MatTableDataSource<BookData>(data);
+		});
+	}
+
+	submit() {
+		this.searchBooks();
+	}
+
+	mouseOverRow(row: BookData) {}
+
+	mouseLeaveRow() {}
+}
