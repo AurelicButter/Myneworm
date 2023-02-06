a65f21749ef18cda3d05e0c9bd6b8dc9c79f19b1
Frederick Katsura <fkatsura@katsurin.com>
2023-02-06 16:45:00 -0500

    Fixed OG Meta Tags From Name to Property Key

--- src/app/services/metadata.service.ts
+++ src/app/services/metadata.service.ts
@@ -11,45 +11,45 @@ export class MetadataService {
 
 	updateMetaTags(title: string, path: string, description?: string, imageURL?: string) {
 		this.titleService.setTitle(`Myneworm - ${title}`);
-		this.metaService.updateTag({ name: "og:title", content: `Myneworm - ${title}` });
-		this.metaService.updateTag({ name: "og:url", content: `https://myneworm.katsurin.com${path}` });
+		this.metaService.updateTag({ property: "og:title", content: `Myneworm - ${title}` });
+		this.metaService.updateTag({ property: "og:url", content: `https://myneworm.katsurin.com${path}` });
 
 		if (description) {
 			this.metaService.updateTag({ name: "description", content: description });
-			this.metaService.updateTag({ name: "og:description", content: description });
+			this.metaService.updateTag({ property: "og:description", content: description });
 		} else {
 			this.metaService.updateTag({
 				name: "description",
 				content: "Track and stay up-to-date on upcoming light novel and manga releases"
 			});
 			this.metaService.updateTag({
-				name: "og:description",
+				property: "og:description",
 				content: "Track and stay up-to-date on upcoming light novel and manga releases"
 			});
 		}
 
 		if (imageURL) {
-			this.metaService.addTag({ name: "og:image", content: imageURL });
+			this.metaService.addTag({ property: "og:image", content: imageURL });
 			this.setImage = true;
 		} else if (this.setImage) {
-			this.metaService.removeTag("name='og:image'");
+			this.metaService.removeTag("property='og:image'");
 			this.setImage = false;
 		}
 	}
 
 	resetMetaTags(title: string) {
 		this.titleService.setTitle(`Myneworm - ${title}`);
-		this.metaService.updateTag({ name: "og:title", content: `Myneworm - ${title}` });
-		this.metaService.updateTag({ name: "og:url", content: `https://myneworm.katsurin.com` });
+		this.metaService.updateTag({ property: "og:title", content: `Myneworm - ${title}` });
+		this.metaService.updateTag({ property: "og:url", content: `https://myneworm.katsurin.com` });
 		this.metaService.updateTag({
 			name: "description",
 			content: "Track and stay up-to-date on upcoming light novel and manga releases"
 		});
 		this.metaService.updateTag({
-			name: "og:description",
+			property: "og:description",
 			content: "Track and stay up-to-date on upcoming light novel and manga releases"
 		});
-		this.metaService.removeTag("name='og:image'");
+		this.metaService.removeTag("property='og:image'");
 		this.setImage = false;
 	}
 }
